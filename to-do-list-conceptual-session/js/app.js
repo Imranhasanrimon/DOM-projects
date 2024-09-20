a8933f7a8358e1a3c07327036728abbf11fdef06
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-20 15:16:53 +0600

    clear all function

--- to-do-list-conceptual-session/js/app.js
+++ to-do-list-conceptual-session/js/app.js
@@ -1,4 +1,22 @@
 let index = 0;
 document.getElementById('add-btn').addEventListener('click', function () {
-    console.log(inputValue('inputValue'));
+    index += 1;
+    const description = inputValue('inputValue');
+    const tr = document.createElement('tr');
+    const i = document.createElement('i');
+    i.setAttribute('class', "fa-solid fa-xmark cursor-pointer text-red-500");
+
+    tr.innerHTML = `
+    <th>${index}</th>
+    <td>${description}</td>
+    <td>${i.outerHTML}</td>
+    `
+    document.getElementById('tbody').append(tr)
+    document.getElementById('inputValue').value = '';
+
+})
+
+// clear-all-btn functionality 
+document.getElementById('clear-all-btn').addEventListener('click', function () {
+    document.getElementById('tbody').remove();
 })
